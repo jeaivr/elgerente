91cf6a85f7aa85771fac7c378fbe4315e1779e72
jeaivr <snezzsp@gmail.com>
2024-10-25 09:42:24 +0200

    update

---
+++ src/sites/landingpage.jsx
@@ -0,0 +1,10 @@
+import styles from "./landingpage.module.css";
+import gerenteImg from "../img/gerente.jpg"
+
+export function LandingPage() {
+  return (
+    <>
+      <center><img src={gerenteImg} style = {{height:"95vh"}} alt="El Gerente"/></center>
+    </>
+  );
+}
