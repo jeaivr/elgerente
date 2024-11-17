646372b16672554949c8820e79f5a37ca442fc9b
Javi <jeaivrv@gmail.com>
2024-11-17 22:38:21 +0100

    final fix

--- src/sites/gerente.jsx
+++ src/sites/gerente.jsx
@@ -1,11 +1,15 @@
 import { useLocation } from "react-router-dom";
+import gerenteImg from "../img/gerente2.jpg"
 
 export function Gerente() {
   const location = useLocation();
   let string = location.pathname.substring(1).replace("/", " y ");
   return (
     <>
-      <center><h1>El gerente es {string}</h1></center>
+      <center>
+        <h1>El gerente es {string}</h1>
+        <img src={gerenteImg} style = {{height:"95vh"}} alt="El Gerente"/>
+      </center>
     </>
   );
 }
